60dee7e27410d99a2d61bfa50ce27d832daf80c6
Prasanthan16 <45173632+Prasanthan16@users.noreply.github.com>
2020-07-08 20:10:34 +0530

    changed the port
    changed the port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const fs = require('fs')
-const port = 4000;
+const port = 5000;
 
 const server = http.createServer(function (req, res) {
     res.writeHead(200, { 'content-type': 'text/html' })
@@ -8,12 +8,14 @@ const server = http.createServer(function (req, res) {
         if (error) {
             res.writeHead(404)
             res.write('error:file not found')
+        
         } else{
             res.write(data)
+            
         }
-         })
-
-    })
+        res.end()
+    });
+});
 
 server.listen(port, function(error) {
     if (error) {
